cb81ebf6527e73baf845e940041f374c2dba37eb
Shevchenko Dmitry <venchesters.2002@gmail.com>
2020-10-20 10:43:44 +0300

    Update getData.js

--- js/getData.js
+++ js/getData.js
@@ -6,7 +6,7 @@ const PARAM = {
 
 
 export const getData = {
-    url: 'database/database.json',
+    url: './database/database.json',
     async getData(url) {
         const responce = await fetch(url);
         if(!responce.ok) {
